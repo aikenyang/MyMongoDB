f55ea7aaa05f1a70f981db96d90b29349a13a9ad
Kenneth Yang (DCS-IAS-TW) <kenneth_yang@tw.trendnet.org>
2016-02-24 11:26:05 +0800

    2/24 2. re-download form chef supermarket, the mongodb cookbook

--- mongodb/test/integration/default/serverspec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'awsmongo-2-6-11::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-end
